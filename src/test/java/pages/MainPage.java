448b0c38c8e8c9296b47fc128a5474bcd637080b
alextai.qa <alextai.qa@gmail.com>
2026-03-02 13:47:55 -0800

    implemented MainPage clicks in the MainPage pom
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -5,7 +5,10 @@ import utils.DriverUtils;
 
 public class MainPage extends BasePage{
 
+    private final By contactButtonXPath = By.xpath("//a[normalize-space()='Contact']");
+    private final By aboutUsButtonXpath = By.xpath("//a[normalize-space()='About us']");
     private final By logInButtonCSS = By.cssSelector("#login2");
+    private final By signUpButtonCSS = By.cssSelector("#signin2");
     private final By welcomeMessageCSS = By.cssSelector("#nameofuser");
 
     public MainPage(DriverUtils driverUtils){
@@ -18,9 +21,24 @@ public class MainPage extends BasePage{
         openPage(url);
     }
 
+    public void clickOnTheContactButton() {
+        log.info("Clicking on the contact button on the main page");
+        driverUtils.click(contactButtonXPath);
+    }
+
+    public void clickOnTheAboutUsButton() {
+        log.info("Clicking on the about us button on the main page");
+        driverUtils.click(aboutUsButtonXpath);
+    }
+
     public void clickOnTheLogInButton() {
         log.info("Clicking on the log in button on the main page");
         driverUtils.click(logInButtonCSS);
+    }
+
+    public void clickOnTheSignUpButton() {
+        log.info("Clicking on the sign up button on the main page");
+        driverUtils.click(signUpButtonCSS);
     }
 
     public String getWelcomeMessage() {
