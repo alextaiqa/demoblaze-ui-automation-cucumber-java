8eb85d0f51d7f59f4317a22a888b1718a8305528
alextai.qa <alextai.qa@gmail.com>
2026-03-02 14:49:48 -0800

    added isDisplayed, getActualHeader
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/ContactModal.java
+++ src/test/java/pages/ContactModal.java
@@ -1,11 +1,13 @@
 package pages;
 
+import org.openqa.selenium.By;
 import utils.DriverUtils;
 
 public class ContactModal extends BasePage {
 
     //selectors
-
+    private final By contactModalCSS = By.cssSelector("#exampleModal");
+    private final By contactModalHeaderCSS = By.cssSelector("#exampleModalLabel");
 
     //constructor
     public ContactModal(DriverUtils driverUtils) {
@@ -13,8 +15,21 @@ public class ContactModal extends BasePage {
     }
 
     //methods
+    public boolean isDisplayed() {
+        log.info("Checking if the contact modal is displayed");
+        return driverUtils.isElementDisplayed(contactModalCSS);
+    }
+
+    public String getActualHeader() {
+        log.info("Getting the actual header of the 'Contact' modal");
+        return driverUtils.getText(contactModalHeaderCSS).trim();
+    }
 
     //test data getters - used for assertions in steps
+    public String getExpectedHeader() {
+        log.info("Getting the expected header of the 'Contact' modal");
+        return data.get("contactModalHeader");
+    }
 
 
 }
