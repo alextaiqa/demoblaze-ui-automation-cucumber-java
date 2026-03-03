b0531b0e3b502bd1c6493370e69064c3d1c5403c
alextai.qa <alextai.qa@gmail.com>
2026-03-02 17:34:23 -0800

    removing TestData from BasePage
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,17 +3,14 @@ package pages;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.DriverUtils;
-import utils.TestData;
 
 public class BasePage {
     protected DriverUtils driverUtils;
     protected final Logger log;
-    protected final TestData data;
 
-    protected BasePage(DriverUtils driverUtils, String filePath) {
+    protected BasePage(DriverUtils driverUtils) {
         this.driverUtils = driverUtils;
         this.log = LoggerFactory.getLogger(this.getClass());
-        this.data = new TestData(filePath);
     }
 
     protected void openPage(String url){
