49e045a020a5c5c4c45d7c8a58047e2ec7fdecf8
alextai.qa <alextai.qa@gmail.com>
2026-03-03 14:27:01 -0800

    removed iSeeTheContactModalHeaderAppears - should be added as a step if a check is needed
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/steps/ContactModalSteps.java
+++ src/test/java/steps/ContactModalSteps.java
@@ -7,18 +7,19 @@ import static org.testng.Assert.*;
 
 public class ContactModalSteps extends BaseSteps {
 
+    //access to POM
     private final ContactModal contactModal;
 
-
+    //constructor
     public ContactModalSteps(ContactModal contactModal) {
         super("testdata/contactModal.yaml");
         this.contactModal = contactModal;
     }
 
+    //methods
     @Then("I see the contact modal appears")
     public void iSeeTheContactModalAppears() {
         assertTrue(contactModal.isDisplayed(), "The 'Contact' modal is not displayed");
-        iSeeTheContactModalHeaderAppears();
     }
 
     @Then("I see the contact modal header appears")
