5486a7689fea41c29d9286d8b4c6183167c267a2
alextai.qa <alextai.qa@gmail.com>
2026-03-02 16:28:40 -0800

    - implemented modal common behavior on the BaseModal to remove duplicate code
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/BaseModal.java
+++ src/test/java/pages/BaseModal.java
@@ -1,19 +1,36 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.DriverUtils;
 import utils.TestData;
 
-public class BaseModal {
+public abstract class BaseModal {
 
     protected final DriverUtils driverUtils;
     protected final Logger log;
-    protected final TestData data;
 
+    protected abstract By getModalContainer();
+
+    protected abstract By getHeaderLocator();
+
+    protected abstract String getModalName(); //for logs
+
+    //constructor
     public BaseModal(DriverUtils driverUtils, String filePath) {
         this.driverUtils = driverUtils;
         this.log = LoggerFactory.getLogger(this.getClass());
-        this.data = new TestData(filePath);
+        this.data = new TestData(filePath); //migrate all to steps !!!!
+    }
+
+    //methods
+    public boolean isModalDisplayed() {
+        log.info("Checking if the {} modal is displayed", getModalName());
+        return driverUtils.isElementDisplayed(getModalContainer());
+    }
+
+    public String getHeaderText() {
+        return driverUtils.getText(getHeaderLocator());
     }
 }
