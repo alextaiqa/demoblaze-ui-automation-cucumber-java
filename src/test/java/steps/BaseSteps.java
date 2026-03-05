1cc714d54b385aff85f2d168c14bf1bd90e84977
alextai.qa <alextai.qa@gmail.com>
2026-03-04 16:41:32 -0800

    created an assertPageTitle method in BaseSteps - less code in other classes + a centralized assertion behavior for a common title check
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -1,6 +1,9 @@
 package steps;
 
+import pages.BasePage;
 import utils.TestData;
+
+import static org.testng.Assert.assertEquals;
 
 public class BaseSteps {
 
@@ -8,5 +11,10 @@ public class BaseSteps {
 
     public BaseSteps(String filePath) {
         this.data = new TestData(filePath);
+    }
+
+    public void assertPageTitle(BasePage page, String pageName, String expectedTitle) {
+        String actualTitle = page.getTitle();
+        assertEquals(actualTitle, expectedTitle, "'" + pageName + "' page title is not correct");
     }
 }
