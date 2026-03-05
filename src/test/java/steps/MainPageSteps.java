f5cdcd05a08dbc371d2917ee639d1efc2c39c5a6
alextai.qa <alextai.qa@gmail.com>
2026-03-05 15:46:36 -0800

    -created NavBarSteps -moved steps that belong to NavBarSteps from MainPageSteps
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/steps/MainPageSteps.java
+++ src/test/java/steps/MainPageSteps.java
@@ -1,7 +1,6 @@
 package steps;
 
 import io.cucumber.java.en.*;
-import org.testng.Assert;
 import pages.MainPage;
 import utils.DriverUtils;
 
@@ -19,48 +18,6 @@ public class MainPageSteps extends BaseSteps {
     @Given("I open the main page")
     public void iOpenTheMainPage(){
         mainPage.open(data.get("homePageURL"));
-    }
-
-    @And("I click on the nav bar logo link")
-    public void iClickOnTheNavBarLogoLink() {
-        mainPage.clickOnTheNavBarLogoLink();
-    }
-
-    @And("I click on the nav bar home link")
-    public void iClickOnTheNavBarHomeLink() {
-        mainPage.clickOnTheNavBarHomeLink();
-    }
-
-    @When("I click on the nav bar contact link")
-    public void iClickOnTheNavBarContactLink() {
-        mainPage.clickOnTheNavBarContactLink();
-    }
-
-    @When("I click on the nav bar about us link")
-    public void iClickOnTheNavBarAboutUsLink() {
-        mainPage.clickOnTheNavBarAboutUsLink();
-    }
-
-    @When("I click on the nav bar log in link")
-    public void iClickOnTheNavBarLogInLink() {
-        mainPage.clickOnTheNavBarLogInLink();
-    }
-
-    @When("I click on the nav bar sign up link")
-    public void iClickOnTheNavBarSignUpLink() {
-        mainPage.clickOnTheNavBarSignUpLink();
-    }
-
-    @And("I click on the nav bar cart link")
-    public void iClickOnTheNavBarCartLink() {
-        mainPage.clickOnTheNavBarCartLink();
-    }
-
-    @Then("I see a welcome message")
-    public void iSeeAWelcomeMessage() {
-        String actualResult = mainPage.getWelcomeMessage();
-        String expectedResult = data.get("logInWelcomeMessage");
-        Assert.assertEquals(actualResult, expectedResult, "Welcome log in message incorrect");
     }
 
     @Then("I see a correct title for the main page")
---
+++ src/test/java/steps/components/NavBarSteps.java
@@ -0,0 +1,62 @@
+package steps.components;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+import pages.components.NavBar;
+import steps.BaseSteps;
+
+public class NavBarSteps extends BaseSteps {
+
+    private final NavBar navBar;
+
+    //constructor
+    public NavBarSteps(NavBar navBar) {
+        super("testdata/navBar.yaml");
+        this.navBar = navBar;
+    }
+
+    //steps
+    @And("I click on the nav bar logo link")
+    public void iClickOnTheNavBarLogoLink() {
+        navBar.clickOnTheLogoLink();
+    }
+
+    @And("I click on the nav bar home link")
+    public void iClickOnTheNavBarHomeLink() {
+        navBar.clickOnTheHomeLink();
+    }
+
+    @When("I click on the nav bar contact link")
+    public void iClickOnTheNavBarContactLink() {
+        navBar.clickOnTheContactLink();
+    }
+
+    @When("I click on the nav bar about us link")
+    public void iClickOnTheNavBarAboutUsLink() {
+        navBar.clickOnTheAboutUsLink();
+    }
+
+    @When("I click on the nav bar log in link")
+    public void iClickOnTheNavBarLogInLink() {
+        navBar.clickOnTheLogInLink();
+    }
+
+    @When("I click on the nav bar sign up link")
+    public void iClickOnTheNavBarSignUpLink() {
+        navBar.clickOnTheSignUpLink();
+    }
+
+    @And("I click on the nav bar cart link")
+    public void iClickOnTheNavBarCartLink() {
+        navBar.clickOnTheCartLink();
+    }
+
+    @Then("I see a welcome message")
+    public void iSeeAWelcomeMessage() {
+        String actualResult = navBar.getWelcomeMessage();
+        String expectedResult = data.get("logInWelcomeMessage");
+        Assert.assertEquals(actualResult, expectedResult, "Welcome log in message incorrect");
+    }
+}
