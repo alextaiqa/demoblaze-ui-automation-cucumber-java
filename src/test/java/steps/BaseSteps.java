8e824e692d2f9cc812b62ea7c1a04c65938b8c24
alextai.qa <alextai.qa@gmail.com>
2026-03-05 15:45:41 -0800

    added comments for clarifications
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -1,6 +1,7 @@
 package steps;
 
 import pages.BasePage;
+import pages.components.NavBar;
 import utils.TestData;
 
 import static org.testng.Assert.assertEquals;
@@ -9,10 +10,12 @@ public class BaseSteps {
 
     protected final TestData data;
 
+    //constructor
     public BaseSteps(String filePath) {
         this.data = new TestData(filePath);
     }
 
+    //helper methods
     public void assertPageTitle(BasePage page, String pageName, String expectedTitle) {
         String actualTitle = page.getTitle();
         assertEquals(actualTitle, expectedTitle, "'" + pageName + "' page title is not correct");
