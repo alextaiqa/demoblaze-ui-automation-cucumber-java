751bee4fac4ed140099b0b8e20ab123d4d30d4a7
alextai.qa <alextai.qa@gmail.com>
2026-03-04 16:48:29 -0800

    - connected CartPage to BasePage - created its constructor - implemented the name getter required by BasePage - implemented isPlaceOrderButtonDisplayed
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,4 +1,27 @@
 package pages;
 
-public class CartPage {
+import org.openqa.selenium.By;
+import utils.DriverUtils;
+
+public class CartPage extends BasePage {
+
+    //selectors
+    private final By placeOrderButton = By.cssSelector(".btn.btn-success");
+
+    //constructor
+    public CartPage(DriverUtils driverUtils) {
+        super(driverUtils);
+    }
+
+    //methods
+    public boolean isPlaceOrderButtonDisplayed() {
+        log.info("Checking if the 'Place Order' button is displayed");
+        return driverUtils.isElementDisplayed(placeOrderButton);
+    }
+
+    //getters
+    @Override
+    public String getPageName() {
+        return "Cart";
+    }
 }
