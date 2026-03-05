d5453977a75c114aaeca347438717c9722ce7cc4
alextai.qa <alextai.qa@gmail.com>
2026-03-05 14:34:55 -0800

    - created clickOnTheNavBarLogoLink - naming changes to reflect links belong to the navBar
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/pages/MainPage.java
+++ src/test/java/pages/pages/MainPage.java
@@ -1,11 +1,13 @@
-package pages;
+package pages.pages;
 
 import org.openqa.selenium.By;
+import pages.BasePage;
 import utils.DriverUtils;
 
 public class MainPage extends BasePage {
 
     //selectors
+    private final By navBarLogoID = By.id("nava");
     private final By homeButtonXPath = By.xpath("//div[@id='navbarExample']//a[contains(normalize-space(),'Home')]");
     private final By contactButtonXPath = By.xpath("//a[normalize-space()='Contact']");
     private final By aboutUsButtonXpath = By.xpath("//a[normalize-space()='About us']");
@@ -21,32 +23,37 @@ public class MainPage extends BasePage {
     }
 
     //methods
-    public void clickOnTheHomeButton() {
+    public void clickOnTheNavBarLogoLink() {
+        log.info("Clicking on the nav bar logo");
+        driverUtils.click(navBarLogoID);
+    }
+
+    public void clickOnTheNavBarHomeLink() {
         log.info("Clicking on the 'Home' button on the nav bar");
         driverUtils.click(homeButtonXPath);
     }
 
-    public void clickOnTheContactButton() {
+    public void clickOnTheNavBarContactLink() {
         log.info("Clicking on the 'Contact' button on the nav bar");
         driverUtils.click(contactButtonXPath);
     }
 
-    public void clickOnTheAboutUsButton() {
+    public void clickOnTheNavBarAboutUsLink() {
         log.info("Clicking on the 'About us' button on the nav bar");
         driverUtils.click(aboutUsButtonXpath);
     }
 
-    public void clickOnTheCartButton() {
+    public void clickOnTheNavBarCartLink() {
         log.info("Clicking on the 'Cart' button on the nav bar");
         driverUtils.click(cartButtonID);
     }
 
-    public void clickOnTheLogInButton() {
+    public void clickOnTheNavBarLogInLink() {
         log.info("Clicking on the 'Log in' button on the nav bar");
         driverUtils.click(logInButtonID);
     }
 
-    public void clickOnTheSignUpButton() {
+    public void clickOnTheNavBarSignUpLink() {
         log.info("Clicking on the 'Sign up' button on the nav bar");
         driverUtils.click(signUpButtonID);
     }
