cb23504f943c40525f3ac1004417611ea82dfa29
alextai.qa <alextai.qa@gmail.com>
2026-03-05 18:05:55 -0800

    -added parallel running -changed the names of the reports
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/runners/MainPageRunnerTest.java
+++ src/test/java/runners/MainPageRunnerTest.java
@@ -2,17 +2,22 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"steps", "hooks", "context", "driver", "pages", "utils"},
         tags = "@mainPage", //logInModal, mainPage, cart
         plugin = {"pretty",
-                "html:target/cucumber.html",
-                "json:target/cucumber.json"
+                "html:target/cucumber-mainPage.html",
+                "json:target/cucumber-mainPage.json"
         }, monochrome = true //what is this?
 )
 
-public class TestRunner extends AbstractTestNGCucumberTests {
-
+public class MainPageRunnerTest extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
