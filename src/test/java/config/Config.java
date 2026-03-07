2cd07282988da9ee0f012b890fc825fd4f1155b8
alextai.qa <alextai.qa@gmail.com>
2026-03-07 02:49:48 -0800

    -added getBrowser() -added isHeadless()
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/config/Config.java
+++ src/test/java/config/Config.java
@@ -11,6 +11,14 @@ public class Config {
         this.data = new TestData("config/config.yaml");
     }
 
+    public String getBrowser() {
+        return data.get("browserChrome");
+    }
+
+    public boolean isHeadless() { //"true" is used on a ci server
+        return Boolean.parseBoolean(data.get("headless"));
+    }
+
     public Duration getRegularWait() {
         return Duration.ofSeconds(Integer.parseInt(data.get("explicit.regular.wait")));
     }
