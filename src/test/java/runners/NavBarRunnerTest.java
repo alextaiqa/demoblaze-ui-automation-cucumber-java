a7b030f3abfc260b2a582bae75aba146fd342761
alextai.qa <alextai.qa@gmail.com>
2026-03-07 02:50:59 -0800

    tag and glue updates
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/runners/NavBarRunnerTest.java
+++ src/test/java/runners/NavBarRunnerTest.java
@@ -5,9 +5,9 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue = {"steps", "hooks", "context", "driver", "pages", "utils"},
-        tags = "@navBar",
+        features = "src/test/resources/features/components",
+        glue = {"hooks", "steps", "context"},
+        tags = "@navBar and not @bug",
         plugin = {"pretty",
                 "html:target/cucumber-navBar.html",
                 "json:target/cucumber-navBar.json"
