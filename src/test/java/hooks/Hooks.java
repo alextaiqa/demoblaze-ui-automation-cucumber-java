09c8db93ca88d63218316b51a0327384a79f90af
alextai.qa <alextai.qa@gmail.com>
2026-03-11 01:55:14 -0700

    - now can run chrome and firefox via jenkins (sequentially) - working on reports with a browser name - working on a mixed report
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -6,8 +6,6 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import factory.DriverFactory;
-import io.cucumber.testng.CucumberOptions;
-import io.qameta.allure.Allure;
 import org.openqa.selenium.*;
 
 import org.slf4j.Logger;
@@ -15,13 +13,6 @@ import org.slf4j.LoggerFactory;
 import utils.DriverUtils;
 import utils.ScreenshotUtils;
 
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = {"hooks", "steps", "context"},
-        tags = "@ui",
-        plugin = {"pretty",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
-)
 
 public class Hooks {
 
@@ -42,8 +33,6 @@ public class Hooks {
 
     @Before
     public void setup() {
-        String browser = System.getProperty("browser", "chrome"); // default
-        Allure.label("browser", browser); // label tests by browser
         WebDriver driver = driverFactory.createDriver();
         DriverManager.setDriver(driver);
         testContext.setDriver(driver);
