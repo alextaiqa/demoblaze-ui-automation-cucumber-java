1b896da151413006e6ab1eea5f3fc67a70bf2d50
alextai.qa <alextai.qa@gmail.com>
2026-03-13 08:08:02 -0700

    implemented methods and added selectors for the steps for new TCs (for previous commit)
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/components/modals/ContactModal.java
+++ src/test/java/pages/components/modals/ContactModal.java
@@ -8,6 +8,10 @@ public class ContactModal extends BaseModal {
     //selectors
     private final By contactModalCSS = By.cssSelector("#exampleModal");
     private final By contactModalHeaderCSS = By.cssSelector("#exampleModalLabel");
+    private final By contactModalEmailID = By.id("recipient-email");
+    private final By contactModalNameID = By.id("recipient-name");
+    private final By contactModalMessageID = By.id("message-text");
+    private final By contactModalConfirmationButtonCSS = By.cssSelector("button[onclick='send()']");
 
     //constructor
     public ContactModal(DriverUtils driverUtils) {
@@ -15,8 +19,36 @@ public class ContactModal extends BaseModal {
     }
 
     //methods
+    public void enterAValidEmail(String data) {
+        log.info("Entering a valid email in the 'Contact' modal");
+        driverUtils.type(contactModalEmailID, data);
+    }
 
-    //getters for common methods
+    public void enterAValidName(String data) {
+        log.info("Entering a valid name in the 'Contact' modal");
+        driverUtils.type(contactModalNameID, data);
+    }
+
+    public void enterAValidMessage(String data) {
+        log.info("Entering a valid message in the 'Contact' modal");
+        driverUtils.type(contactModalMessageID, data);
+    }
+
+    public void clickOnTheConfirmationButton() {
+        log.info("Clicking on the 'Contact' modal confirmation button");
+        driverUtils.click(contactModalConfirmationButtonCSS);
+    }
+
+    public String getAlertText() {
+        log.info("Getting a 'Contact' modal alert message");
+        return driverUtils.getAlertText();
+    }
+
+    public void enterAnInvalidEmailWithoutDomain(String data) {
+        driverUtils.type(contactModalEmailID, data);
+    }
+
+    //getters for base methods
     @Override
     protected By getModalContainer() {
         return contactModalCSS;
