ecff405c09e5c586a8e61aced07e28f37f4f633f
alextai.qa <alextai.qa@gmail.com>
2026-03-13 03:15:02 -0700

    -implemented clickOnThePlaceOrderButton -added a placeOrderModalContainer selector
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -7,6 +7,7 @@ public class CartPage extends BasePage {
 
     //selectors
     private final By placeOrderButton = By.cssSelector(".btn.btn-success");
+    private final By placeOrderModalContainer = By.id("orderModal");
 
     //constructor
     public CartPage(DriverUtils driverUtils) {
@@ -17,6 +18,16 @@ public class CartPage extends BasePage {
     public boolean isPlaceOrderButtonDisplayed() {
         log.info("Checking if the 'Place Order' button is displayed");
         return driverUtils.isElementDisplayed(placeOrderButton);
+    }
+
+    public void clickOnThePlaceOrderButton() {
+        log.info("Clicking on the 'Place Order' button");
+        driverUtils.click(placeOrderButton);
+    }
+
+    public boolean isPlaceOrderModalDisplayed() {
+        log.info("Checking if the 'Place Order' modal container is displayed");
+        return driverUtils.isElementDisplayed(placeOrderModalContainer);
     }
 
     //getters
