87fb3de7f3fe11648ec6ac838d212b36cb8c9e63
alextai.qa <alextai.qa@gmail.com>
2026-03-22 18:20:35 -0700

    added a few comments for clarity
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/flows/AuthFlow.java
+++ src/test/java/flows/AuthFlow.java
@@ -5,14 +5,19 @@ import pages.components.modals.LoginModal;
 
 public class AuthFlow {
 
+    //global vars
     private final NavBar navBar;
     private final LoginModal loginModal;
 
+
+    //constructor
     public AuthFlow(NavBar navBar, LoginModal loginModal) {
         this.navBar = navBar;
         this.loginModal = loginModal;
     }
 
+
+    //methods
     public void login(String username, String password) {
         navBar.clickOnTheLogInButton();
         loginModal.enterACorrectUsername(username);
