760a0f144920c0a187498ff85f8f48abd59c3b56
alextai.qa <alextai.qa@gmail.com>
2026-03-23 02:28:57 -0700

    - switched assertTrue to assertEquals - instead of getting a boolean, getting a List and comparing in steps, not POM - makes debugging easier
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/steps/components/FootnoteSteps.java
+++ src/test/java/steps/components/FootnoteSteps.java
@@ -2,6 +2,7 @@ package steps.components;
 
 import io.cucumber.java.en.Then;
 import pages.components.Footnote;
+import utils.TestData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,17 +12,20 @@ import static org.testng.Assert.*;
 public class FootnoteSteps {
 
     private final Footnote footnote;
+    private final TestData data;
 
     public FootnoteSteps(Footnote footnote) {
         this.footnote = footnote;
+        this.data = new TestData("testdata/footnote.yaml");
     }
 
     @Then("I see a footnote is displayed")
     public void iSeeAFootnoteIsDisplayed() {
-
-        ArrayList<String> expectedHeaders = new ArrayList<>(List.of("Get in Touch", "About Us", "About Us", "PRODUCT STORE"));
         assertTrue(footnote.isDisplayed(), "Footnote is not displayed");
-        assertTrue(footnote.areFootnoteHeadersCorrect(expectedHeaders));
+
+        List<String> actualHeaders = footnote.getFootnoteHeaders();
+        List<String> expectedHeaders = data.getStringList("headers");
+        assertEquals(actualHeaders, expectedHeaders, "Displayed footnote headers are incorrect");
     }
 
     @Then("I see a copyright ribbon is displayed")
