641a538b38a1a79ff84846274aaca6142faccc52
alextai.qa <alextai.qa@gmail.com>
2026-03-22 18:30:19 -0700

    - created a FootnoteSteps constructor - implemented the 'iSeeAFootnoteIsDisplayed' method implemented the 'iSeeACopyrightRibbonIsDisplayed' method
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/steps/components/FootnoteSteps.java
+++ src/test/java/steps/components/FootnoteSteps.java
@@ -1,4 +1,31 @@
 package steps.components;
 
+import io.cucumber.java.en.Then;
+import pages.components.Footnote;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
 public class FootnoteSteps {
+
+    private final Footnote footnote;
+
+    public FootnoteSteps(Footnote footnote) {
+        this.footnote = footnote;
+    }
+
+    @Then("I see a footnote is displayed")
+    public void iSeeAFootnoteIsDisplayed() {
+
+        ArrayList<String> expectedHeaders = new ArrayList<>(List.of("Get in Touch", "About Us", "About Us", "PRODUCT STORE"));
+        assertTrue(footnote.isDisplayed(), "Footnote is not displayed");
+        assertTrue(footnote.areFootnoteHeadersCorrect(expectedHeaders));
+    }
+
+    @Then("I see a copyright ribbon is displayed")
+    public void iSeeACopyrightRibbonIsDisplayed() {
+        assertTrue(footnote.isCopyrightRibbonDisplayed(), "Copyright ribbon is not displayed");
+    }
 }
