9f0be9c12ea77cc6ef4beca663489a3b0d6d575a
alextai.qa <alextai.qa@gmail.com>
2026-04-08 14:31:17 -0700

    - added selectors (mentioned in steps commit accidentally). Actually done in this file
    - implemented clicks for all device categories on the main page
    
    - implemented the "isDeviceCategorySizeBiggerThanNine" method - one selector for all devices displayed - device list changes on clicking on different categories, hence one method
    
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -8,6 +8,15 @@ public class MainPage extends BasePage {
     //selectors
 
     private final By categoriesContainerID = By.id("contcont");
+    private final By defaultCategoryButtonID = By.id("cat");
+    private final By phonesCategoryButtonXPath =
+            By.xpath("//a[@id='cat']/../a[contains(normalize-space(),'Phones')]");
+    private final By laptopsCategoryButtonXPath =
+            By.xpath("//a[@id='cat']/../a[contains(normalize-space(),'Laptops')]");
+    private final By monitorsCategoryButtonXPath =
+            By.xpath("//a[@id='cat']/../a[contains(normalize-space(),'Monitors')]");
+    private final By deviceCategoryTableXPath = By.xpath("//div[@id='tbodyid']/div");
+
 
     //constructor
     public MainPage(DriverUtils driverUtils) {
@@ -18,6 +27,31 @@ public class MainPage extends BasePage {
     public boolean isCategoriesContainerDisplayed() {
         log.info("Checking if the 'Categories' container is displayed");
         return driverUtils.isElementDisplayed(categoriesContainerID);
+    }
+
+    public void clickOnTheDefaultCategoryButton() {
+        log.info("Clicking on the 'Default' device category");
+        driverUtils.click(defaultCategoryButtonID);
+    }
+
+    public void clickOnThePhonesCategoryButton() {
+        log.info("Clicking on the 'Phones' device category");
+        driverUtils.click(phonesCategoryButtonXPath);
+    }
+
+    public void clickOnTheLaptopsCategoryButton() {
+        log.info("Clicking on the 'Laptops' device category");
+        driverUtils.click(laptopsCategoryButtonXPath);
+    }
+
+    public void clickOnTheMonitorsCategoryButton() {
+        log.info("Clicking on the 'Monitors' device category");
+        driverUtils.click(monitorsCategoryButtonXPath);
+    }
+
+    public boolean isDeviceCategorySizeBiggerThanNine() {
+        log.info("Checking if the category has more than 9 items");
+        return driverUtils.getVisibleElements(deviceCategoryTableXPath).size() <= 9;
     }
 
     //methods for common behavior
