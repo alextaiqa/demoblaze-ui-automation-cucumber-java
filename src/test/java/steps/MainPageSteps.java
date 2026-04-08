8bccd8df01b6900e8c75b9ccea42bd012a21a4df
alextai.qa <alextai.qa@gmail.com>
2026-04-08 14:28:14 -0700

    - implemented the clicks for the categories  buttons on the main page
    - added selectors for the buttons, and the device container
    
     - implemented assertions for all categories - for the "9 items displayed?" check
    
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/steps/MainPageSteps.java
+++ src/test/java/steps/MainPageSteps.java
@@ -29,4 +29,49 @@ public class MainPageSteps extends BaseSteps {
     public void iSeeACategoriesContainer() {
         assertTrue(mainPage.isCategoriesContainerDisplayed());
     }
+
+    @And("I click on the Default category on the main page")
+    public void iClickOnTheDefaultCategoryOnTheMainPage() {
+        mainPage.clickOnTheDefaultCategoryButton();
+    }
+
+    @And("I click on the Phones category on the main page")
+    public void iClickOnThePhonesCategoryOnTheMainPage() {
+        mainPage.clickOnThePhonesCategoryButton();
+    }
+
+    @And("I click on the Laptops category on the main page")
+    public void iClickOnTheLaptopsCategoryOnTheMainPage() {
+        mainPage.clickOnTheLaptopsCategoryButton();
+    }
+
+    @And("I click on the Monitors category on the main page")
+    public void iClickOnTheMonitorsCategoryOnTheMainPage() {
+        mainPage.clickOnTheMonitorsCategoryButton();
+    }
+
+
+    @Then("I see no more than nine items in the Default category on the main page")
+    public void iSeeNoMoreThanNineItemsInTheDefaultCategoryOnTheMainPage() {
+        assertTrue(mainPage.isDeviceCategorySizeBiggerThanNine(),
+                "The 'Default' category has more than 9 items on the page");
+    }
+
+    @Then("I see no more than nine items in the Phones category on the main page")
+    public void iSeeNoMoreThanNineItemsInThePhonesCategoryOnTheMainPage() {
+        assertTrue(mainPage.isDeviceCategorySizeBiggerThanNine(),
+                "The 'Phones' category has more than 9 items on the page");
+    }
+
+    @Then("I see no more than nine items in the Laptops category on the main page")
+    public void iSeeNoMoreThanNineItemsInTheLaptopsCategoryOnTheMainPage() {
+        assertTrue(mainPage.isDeviceCategorySizeBiggerThanNine(),
+                "The 'Laptops' category has more than 9 items on the page");
+    }
+
+    @Then("I see no more than nine items in the Monitors category on the main page")
+    public void iSeeNoMoreThanNineItemsInTheMonitorsCategoryOnTheMainPage() {
+        assertTrue(mainPage.isDeviceCategorySizeBiggerThanNine(),
+                "The 'Monitors' category has more than 9 items on the page");
+    }
 }
