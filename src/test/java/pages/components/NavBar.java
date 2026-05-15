14754058fcd6e6cc05ef4c949a56c8b9a0294930
alextai.qa <alextai.qa@gmail.com>
2026-05-14 19:53:51 -0700

    - made multiple comment adjustments
    - implemented a singular method "clickOnTheButton" that accepts parameters - removes code duplication
    
    - removed multiple duplicate methods because of the method mentioned above
    
    - initialized and populated a map "navBarButtons" - keeps selectors of all the main nav bar buttons
    
    - implemented a helper method that gets the selectors of the main nav bar buttons via a parameter
    
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/components/NavBar.java
+++ src/test/java/pages/components/NavBar.java
@@ -6,9 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.DriverUtils;
 
+import java.util.Map;
+
 public class NavBar {
 
-    //selectors
+    // =======================
+// SELECTORS
+// =======================
     private final By navBarCSS = By.cssSelector(".navbar.navbar-toggleable-md.bg-inverse");
     private final By logoID = By.id("nava");
     private final By homeButtonXPath = By.xpath("//div[@id='navbarExample']//a[contains(normalize-space(),'Home')]");
@@ -20,65 +24,51 @@ public class NavBar {
     private final By welcomeMessageID = By.id("nameofuser");
     private final By logOutButtonID = By.id("logout2");
 
-    //global variables
+    // =======================
+// MAP(RESOLVER)
+// =======================
+    //stores main nav bar buttons
+    private final Map<String, By> navBarButtons = Map.of(
+            "logo", logoID,
+            "home", homeButtonXPath,
+            "contact", contactButtonXPath,
+            "about us", aboutUsButtonXpath,
+            "cart", cartButtonID,
+            "log in", logInButtonID,
+            "log out", logOutButtonID,
+            "sign up", signUpButtonID);
+
+    // =======================
+// GLOBAL VARIABLE
+// =======================
     private final DriverUtils driverUtils;
     private final Logger log;
 
-    //constructor
+    // =======================
+// CONSTRUCTOR
+// =======================
     public NavBar(DriverUtils driverUtils) {
         this.driverUtils = driverUtils;
         this.log = LoggerFactory.getLogger(this.getClass());
     }
 
-    //methods
+    // =======================
+// METHODS
+// =======================
     public boolean isDisplayed() {
         log.info("Checking if the nav bar is displayed");
         return driverUtils.isElementDisplayed(navBarCSS);
     }
 
-    public void clickOnTheLogo() {
-        log.info("Clicking on the nav bar logo");
-        driverUtils.click(logoID);
-    }
-
-    public void clickOnTheHomeButton() {
-        log.info("Clicking on the 'Home' button on the nav bar");
-        driverUtils.click(homeButtonXPath);
-    }
-
-    public void clickOnTheContactButton() {
-        log.info("Clicking on the 'Contact' button on the nav bar");
-        driverUtils.click(contactButtonXPath);
-    }
-
-    public void clickOnTheAboutUsButton() {
-        log.info("Clicking on the 'About us' button on the nav bar");
-        driverUtils.click(aboutUsButtonXpath);
-    }
-
-    public void clickOnTheCartButton() {
-        log.info("Clicking on the 'Cart' button on the nav bar");
-        driverUtils.click(cartButtonID);
-    }
-
-    public void clickOnTheLogInButton() {
-        log.info("Clicking on the 'Log in' button on the nav bar");
-        driverUtils.click(logInButtonID);
-    }
-
-    public void clickOnTheSignUpButton() {
-        log.info("Clicking on the 'Sign up' button on the nav bar");
-        driverUtils.click(signUpButtonID);
+    //KEEP
+    public void clickOnTheButton(String button) {
+        log.info("Clicking on the '{}' button on the nav bar", button);
+        driverUtils.click(getButtonSelector(button));
     }
 
     public String getWelcomeMessage() {
         log.info("Getting an actual welcome message for a logged in user");
         return driverUtils.getText(welcomeMessageID);
-    }
-
-    public void clickOnTheLogOutButton() {
-        log.info("Clicking on the 'Log out' button on the nav bar");
-        driverUtils.click(logOutButtonID);
     }
 
     public String getSignUpButtonText() {
@@ -97,17 +87,16 @@ public class NavBar {
         return driverUtils.getCSSValue(getButtonSelector(button), "color");
     }
 
-    //helpers
-    public By getButtonSelector(String button) {
+    // =======================
+// HELPERS
+// =======================
+    //gets selectors of the main nav bar buttons
+    private By getButtonSelector(String button) {
         button = button.toLowerCase();
-        return switch (button) {
-            case "home" -> homeButtonXPath;
-            case "contact" -> contactButtonXPath;
-            case "about us" -> aboutUsButtonXpath;
-            case "cart" -> cartButtonID;
-            case "log in" -> logInButtonID;
-            case "sign up" -> signUpButtonID;
-            default -> throw new NoSuchElementException("Unexpected value: " + button);
-        };
+        By selector = navBarButtons.get(button);
+        if (selector == null) {
+            throw new NoSuchElementException("Nav bar button not found. Received: '" + button + "'.");
+        }
+        return selector;
     }
 }
