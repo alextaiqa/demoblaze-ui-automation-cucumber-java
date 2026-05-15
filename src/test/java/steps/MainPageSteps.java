f568ad2ae655dfee82d10c2960003964deb58c7a
alextai.qa <alextai.qa@gmail.com>
2026-05-15 16:53:54 -0700

    - removed duplicate methods
    - implemented several parameterized methods in place of the removed ones - reduced code duplicates
    
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/steps/MainPageSteps.java
+++ src/test/java/steps/MainPageSteps.java
@@ -30,66 +30,21 @@ public class MainPageSteps extends BaseSteps {
         assertTrue(mainPage.isCategoriesContainerDisplayed());
     }
 
-    @And("I click on the Default category on the main page")
-    public void iClickOnTheDefaultCategoryOnTheMainPage() {
-        mainPage.clickOnTheDefaultCategoryButton();
+    @And("I click on the {string} category on the main page")
+    public void iClickOnTheCategoryOnTheMainPage(String category) {
+        mainPage.clickOnTheCategoryButton(category);
     }
 
-    @And("I click on the Phones category on the main page")
-    public void iClickOnThePhonesCategoryOnTheMainPage() {
-        mainPage.clickOnThePhonesCategoryButton();
-    }
-
-    @And("I click on the Laptops category on the main page")
-    public void iClickOnTheLaptopsCategoryOnTheMainPage() {
-        mainPage.clickOnTheLaptopsCategoryButton();
-    }
-
-    @And("I click on the Monitors category on the main page")
-    public void iClickOnTheMonitorsCategoryOnTheMainPage() {
-        mainPage.clickOnTheMonitorsCategoryButton();
-    }
-
-    @Then("I see no more than nine items in the Default category on the main page")
-    public void iSeeNoMoreThanNineItemsInTheDefaultCategoryOnTheMainPage() {
+    @Then("I see no more than nine items in the {string} category on the main page")
+    public void iSeeNoMoreThanNineItemsInTheDefaultCategoryOnTheMainPage(String category) {
         assertTrue(mainPage.isDeviceCategorySizeGreaterThanNine(),
-                "The 'Default' category has more than 9 items on the page");
+                "The '" + category + "' category has more than 9 items on the page");
     }
 
-    @Then("I see no more than nine items in the Phones category on the main page")
-    public void iSeeNoMoreThanNineItemsInThePhonesCategoryOnTheMainPage() {
-        assertTrue(mainPage.isDeviceCategorySizeGreaterThanNine(),
-                "The 'Phones' category has more than 9 items on the page");
-    }
-
-    @Then("I see no more than nine items in the Laptops category on the main page")
-    public void iSeeNoMoreThanNineItemsInTheLaptopsCategoryOnTheMainPage() {
-        assertTrue(mainPage.isDeviceCategorySizeGreaterThanNine(),
-                "The 'Laptops' category has more than 9 items on the page");
-    }
-
-    @Then("I see no more than nine items in the Monitors category on the main page")
-    public void iSeeNoMoreThanNineItemsInTheMonitorsCategoryOnTheMainPage() {
-        assertTrue(mainPage.isDeviceCategorySizeGreaterThanNine(),
-                "The 'Monitors' category has more than 9 items on the page");
-    }
-
-    @Then("I see the first image of the preview gallery on the main page")
-    public void iSeeTheFirstImageOfThePreviewGalleryOnTheMainPage() {
-        assertTrue(mainPage.isPreviewGalleryImageDisplayed(1),
-                "The first image of the preview gallery is not displayed");
-    }
-
-    @Then("I see the second image of the preview gallery on the main page")
-    public void iSeeTheSecondImageOfThePreviewGalleryOnTheMainPage() {
-        assertTrue(mainPage.isPreviewGalleryImageDisplayed(2),
-                "The second image of the preview gallery is not displayed");
-    }
-
-    @Then("I see the third image of the preview gallery on the main page")
-    public void iSeeTheThirdImageOfThePreviewGalleryOnTheMainPage() {
-        assertTrue(mainPage.isPreviewGalleryImageDisplayed(3),
-                "The third image of the preview gallery is not displayed");
+    @Then("I see the {int} image of the preview gallery on the main page")
+    public void iSeeTheFirstImageOfThePreviewGalleryOnTheMainPage(int imageNumber) {
+        assertTrue(mainPage.isPreviewGalleryImageDisplayed(imageNumber),
+                "The " + imageNumber + " image of the preview gallery is not displayed");
     }
 
     @And("I click on the next button of the preview gallery on the main page")
