e531f2e1b5ff31ab75a686e83df87bf518a5313f
alextai.qa <alextai.qa@gmail.com>
2026-09-24 16:22:09 -0700

    - added selectors for all the fields and buttons of the "Place order" modal
    - completed all functions for field data entering, and clicking. Added logs for all as well
    
    Signed-off-by: alextai.qa <alextai.qa@gmail.com>

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -2,15 +2,24 @@ package pages;
 
 import org.openqa.selenium.By;
 import utils.DriverUtils;
-import utils.TestDataGenerator;
 
 public class CartPage extends BasePage {
-
-    //global selector
 
     //selectors
     private final By placeOrderButton = By.cssSelector(".btn.btn-success");
     private final By placeOrderModalContainer = By.id("orderModal");
+    private final By fullNameModalFieldCSS = By.id("name");
+    private final By countryModalFieldCSS = By.id("country");
+    private final By cityModalFieldCSS = By.id("city");
+    private final By creditCardModalFieldCSS = By.id("card");
+    private final By monthModalFieldCSS = By.id("month");
+    private final By yearModalFieldCSS = By.id("year");
+    private final By purchaseModalButton =
+            By.xpath("//button[normalize-space()='Purchase']");
+    private final By closeModalButton =
+            By.xpath("//div[@id='orderModal']//button[normalize-space()='Close']");
+    private final By modalSweetAlertThankYouMessageXPath =
+            By.xpath("//h2[normalize-space()='Thank you for your purchase!']");
 
     //constructor
     public CartPage(DriverUtils driverUtils) {
@@ -34,37 +43,43 @@ public class CartPage extends BasePage {
     }
 
     public void enterAValidFullNameInThePlaceOrderModal(String fullName) {
-//        log.info();
-//        driverUtils.type(, );
+        log.info("Cart - 'Place order' modal - entering a valid full name: {}", fullName);
+        driverUtils.type(fullNameModalFieldCSS, fullName);
     }
 
     public void enterAValidCountryInThePlaceOrderModal(String country) {
-//        log.info();
-//        driverUtils.type(, );
+        log.info("Cart - 'Place order' modal - entering a valid country");
+        driverUtils.type(countryModalFieldCSS, country);
     }
 
     public void enterAValidCityInThePlaceOrderModal(String city) {
-
+        log.info("Cart - 'Place order' modal - entering a valid city");
+        driverUtils.type(cityModalFieldCSS, city);
     }
 
     public void enterAValidCreditCardInThePlaceOrderModal(String creditCardDigits) {
-
+        log.info("Cart - 'Place order' modal - entering valid credit card digits");
+        driverUtils.type(creditCardModalFieldCSS, creditCardDigits);
     }
 
     public void enterAValidMonthInThePlaceOrderModal(String month) {
-
+        log.info("Cart - 'Place order' modal - entering a valid month");
+        driverUtils.type(monthModalFieldCSS, month);
     }
 
     public void enterAValidYearInThePlaceOrderModal(String year) {
-
+        log.info("Cart - 'Place order' modal - entering a valid year");
+        driverUtils.type(yearModalFieldCSS, year);
     }
 
     public void clickOnThePlaceOrderPurchaseButton() {
-
+        log.info("Cart - 'Place order' modal - clicking on the 'Purchase' button");
+        driverUtils.click(purchaseModalButton);
     }
 
     public String getPurchaseConfirmationMessage() {
-        return "";
+        log.info("Cart - 'Place order' modal - getting a purchase confirmation message");
+        return driverUtils.getText(modalSweetAlertThankYouMessageXPath);
     }
 
 
